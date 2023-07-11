582605d30a6cf3feb3496b84f530971b04569e7e
taylor <taylormyers01@comcast.net>
2023-07-11 13:50:48 -0400

    updated student and partially completed classroom functionality

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,97 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Classroom {
+    private Student[] students;
+
+
+    public Classroom(){
+        students = new Student[30];
+    }
+    public Classroom(int numOfStudents){
+        students = new Student[numOfStudents];
+    }
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+
+
+    //Getters Setters and fun stuff beyond this point
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public double getAverageExamScore(){
+        double average = 0.0;
+        for(Student s : students){
+            average+= s.getAverageExamScore();
+        }
+        average /= students.length;
+        return average;
+    }
+
+    public void addStudent(Student student){
+        Student[] hold = new Student[students.length];
+        for(int i = 0; i < hold.length; i++){
+            if(students[i] == null){
+                hold[i] = student;
+                break;
+            }
+            hold[i] = students[i];
+        }
+        this.students = hold;
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        for(int i = 0; i < students.length; i++){
+            if(students[i].getFirstName().equals(firstName)&& students[i].getLastName().equals(lastName)){
+                students[i] = null;
+            }
+        }
+        Student[] hold = new Student[students.length];
+        int position = 0;
+        int endPosition = students.length-1;
+        for(int i = 0; i < students.length; i++){
+            if(students[i]== null){
+            hold[endPosition] = students[i];
+            endPosition--;
+            } else{
+                hold[position] = students[i];
+                position++;
+            }
+        }
+        students = hold;
+
+    }
+    public static void main(String[] args) {
+        // : Given
+        int maxNumberOfStudents = 4;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+        Student student1 = new Student("James", "Jimmithy", examScores);
+        Student student2 = new Student("Jame", "JimJim", examScores);
+        Student student3 = new Student("Jay", "Jibs", examScores);
+
+        // When
+        classroom.addStudent(student);
+        classroom.addStudent(student1);
+        classroom.addStudent(student2);
+        classroom.addStudent(student3);
+        Student[] preEnrollment = classroom.getStudents();
+        classroom.removeStudent("James", "Jimmithy");
+        Student[] postEnrollment = classroom.getStudents();
+
+        // Then
+        String preEnrollmentAsString = Arrays.toString(preEnrollment);
+        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+
+        System.out.println("===========================");
+        System.out.println(preEnrollmentAsString);
+        System.out.println("===========================");
+        System.out.println(postEnrollmentAsString);
+    }
+
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,89 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores = new ArrayList<>();
+
+    public Student(String firstName, String lastName, Double[] testScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        for(double x : testScores){
+            examScores.add(x);
+        }
+    }
+
+
+
+
+    //Getters and setters below this point
+    public String getExamScores(){
+        StringBuilder output = new StringBuilder();
+        int examNum = 1;
+        for(double x : examScores){
+            output.append(String.format("  Exam %d -> %.0f \n", examNum, x));
+            examNum++;
+        }
+        return output.toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        output.append(String.format("Student name: %s %s\n", firstName, lastName));
+        output.append(String.format("Average score: %.0f\n", getAverageExamScore()));
+        output.append(String.format("Exam Scores:\n%s\n", getExamScores()));
+        return output.toString();
+    }
+
+    public double getAverageExamScore(){
+        double average = 0.0;
+        for(double x: examScores){
+            average+=x;
+        }
+        average /= examScores.size();
+        return average;
+    }
+    public void setExamScore(int examNumber, double newScore){
+        examScores.set(examNumber-1, newScore);
+    }
+
+    public void addExamScore(double examScore){
+        this.examScores.add(examScore);
+    }
+    public int getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public static void main(String[] args) {
+// : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+        // When
+        String output = student.toString();
+
+        // Then
+        System.out.println(output);
+    }
 }
